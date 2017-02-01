b654661bae7b7a7bdd745d18732b5e183a3cc233
oshaman <reg_forall@bigmir.net>
2017-02-01 15:39:21 +0200

    Article 1

--- app/Repositories/ArticlesRepository.php
+++ app/Repositories/ArticlesRepository.php
@@ -7,8 +7,19 @@ use Corp\Article;
 class ArticlesRepository extends Repository {
 	
 	
-	public function __construct(Article $article) {
-		$this->model = $article;
+	public function __construct(Article $articles) {
+		$this->model = $articles;
+	}
+	
+	public function one($alias,$attr = array()) {
+		$article = parent::one($alias,$attr);
+		
+		if($article && !empty($attr)) {
+			$article->load('comments');
+			$article->comments->load('user');
+		}
+		
+		return $article;
 	}
 	
 }
--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,9 @@ Route::resource('articles','ArticlesController',[
 												
 												]
 												
-												]);
-
-Route::get('articles/cat/{cat_alias?}',['uses'=>'ArticleController@index','as'=>'articlesCat']);
+												]);	
+Route::get('articles/cat/{cat_alias?}',['uses'=>'ArticlesController@index','as'=>'articlesCat']);
+																						
 
                                                 
                                                     
